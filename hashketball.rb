9adedb362bab07c2a40ff4a36bc9f5782cca5a63
Erick Villeda <eeverickeev@gmail.com>
2020-09-08 17:20:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -56,7 +56,26 @@ hashketball = {
   :away => {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
-    :players => {}
+    :players => [
+      {:player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2},
+        {:player_name => "",
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => },
+      ]
   }
 }
 end
